c9b0f80a5bbe73eb3ba37d9fc28b4b75ea0ca593
haruka-g <mortndpgj@gmail.com>
2020-01-26 00:45:42 +0900

    マウスオーバー時の実装を追加

--- public/main.js
+++ public/main.js
@@ -1,8 +1,18 @@
-const helloElement = document.getElementById('hello');
-const clickElement = document.getElementById('click');
+const helloElement = document.getElementById("hello");
+const clickElement = document.getElementById("click");
 
-hello.addEventListener('click', (event) => {
-    clickElement.textContent = '';
-    helloElement.textContent = 'こんにちは';
-    helloElement.id = 'ja';
+hello.addEventListener("mouseover", event => {
+  clickElement.textContent = "";
+  helloElement.style.color = "red";
+  helloElement.textContent = "(n_n) /~";
+});
+hello.addEventListener("mouseleave", event => {
+  clickElement.textContent = "↑click↑";
+  helloElement.style.color = "#ff00b3";
+  helloElement.textContent = "Hello!";
+});
+hello.addEventListener("click", event => {
+  clickElement.textContent = "";
+  helloElement.textContent = "こんにちは";
+  helloElement.id = "ja";
 });
