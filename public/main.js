85d19080b61a40e379d23e34bc48d7e4510fea3f
haruka-g <mortndpgj@gmail.com>
2020-01-25 22:41:05 +0900

    hoverイベントを実装

--- public/main.js
+++ public/main.js
@@ -1,9 +1,13 @@
 const helloElement = document.getElementById('hello');
 const clickElement = document.getElementById('click');
 
+hello.addEventListener('mouseover', (event) => {
+    clickElement.textContent = '';
+    helloElement.style.color = 'red';
+});
+
 hello.addEventListener('click', (event) => {
     clickElement.textContent = '';
     helloElement.textContent = 'こんにちは';
     helloElement.id = 'ja';
 });
-
