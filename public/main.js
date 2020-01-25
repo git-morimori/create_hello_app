916cddfe1c6512119439c6b08561c14e8b47e869
haruka-g <mortndpgj@gmail.com>
2020-01-25 22:47:35 +0900

    元に戻した

--- public/main.js
+++ public/main.js
@@ -1,11 +1,6 @@
 const helloElement = document.getElementById('hello');
 const clickElement = document.getElementById('click');
 
-hello.addEventListener('mouseover', (event) => {
-    clickElement.textContent = '';
-    helloElement.style.color = 'red';
-});
-
 hello.addEventListener('click', (event) => {
     clickElement.textContent = '';
     helloElement.textContent = 'こんにちは';
